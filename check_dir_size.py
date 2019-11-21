4b165967ab279e387aa05adddc73a60e5c9bbc91
Steven C <stevenc.github@gmail.com>
2019-11-21 16:02:47 -0500

    Add status messages and conditional checks to set the status

--- check_dir_size.py
+++ check_dir_size.py
@@ -17,51 +17,57 @@ def check_dir_size(dpath):
     """
     checked_size = 0
     total_size = 0
+    check_count = 0
     
     begin_size_chk_msg = f"Checking to see if directory is still growing for:  {os.path.basename(dpath)}"
     logger.info(begin_size_chk_msg)
 
     while True:
-        check_count = 0
         try:     
             for dirpath, dirnames, filenames in os.walk(dpath):
                 for f in filenames:
                     fp = os.path.join(dirpath, f)
                     total_size += os.path.getsize(fp)
                 
-            size_chk_msg = f"\n\n\
-            ================================================================\n \
+            size_chk_msg = f"\n\
+            ================================================================\n\
                         Checking Size for Dir: {os.path.basename(dpath)}\n\
                         Total Dir Size at Start: {checked_size}\n\
                         Total Dir Size at Finish: {total_size}\n\
-            ================================================================\n\
-            \n"
+            ================================================================\
+            "
             logger.info(size_chk_msg)
             check_count += 1
 
+            dir_growing_msg = f"Dir size is still growing for:  {os.path.basename(dpath)}"
+            pause_message = f"Waiting 30 seconds to remeasure dir size."
+            chk_count_message = f"Check Count = {check_count}"
+
             if (total_size != checked_size
-                and check_count <= 6):
+                and check_count < 3):
 
                 checked_size = total_size
                 total_size = 0
 
-                dir_growing_msg = f"Dir size is still growing for:  {os.path.basename(dpath)}"
-                pause_message = f"Wating 30 seconds to remeasure dir size."
-                chk_count_message = f"Check Count = {check_count}"
                 logger.info(chk_count_message)
-                logger.info(dir_growing_msg)
                 logger.info(pause_message)
 
                 time.sleep(30)
-
                 continue
-            elif check_count == 6: 
+
+
+            elif (total_size != checked_size
+                and check_count == 3):
                 directory_value = 1
-                count_end_msg = f"{os.path.basename(dpath)} - dir is still growing after 3min, moving to next dir for archive."
+                count_end_msg = f"{os.path.basename(dpath)} - dir is still growing after 90sec, moving to next dir for archive."
+                logger.info(chk_count_message)
                 logger.info(count_end_msg)
                 break
-            else:
+
+            elif (total_size == checked_size
+                  and check_count > 1):
                 dir_notgrowing_msg = f"{os.path.basename(dpath)}  is ready for archive. End of size check."
+                logger.info(chk_count_message)
                 logger.info(dir_notgrowing_msg)
                 directory_value = 0
                 break
