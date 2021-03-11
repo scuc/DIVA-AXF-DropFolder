041be41dea1edb2639c7e2b0a573d617c56fa78a
Steven C <stevenc.github@gmail.com>
2021-03-11 16:00:49 -0500

    Removes print statements, changes to the logging message

--- archive_queue.py
+++ archive_queue.py
@@ -12,7 +12,6 @@ import filepath_mods as fpmod
 config = config.get_config()
 archive_f = config['paths']['mac_archive_folder']
 drop_f = config['paths']['mac_dropfolder']
-divaname = config['paths']['DIVAName']
 
 logger = logging.getLogger(__name__)
 
@@ -26,7 +25,7 @@ def archiving_check():
         try: 
             alist = [a for a in os.listdir(
                 archive_f) if os.path.isdir(os.path.join(archive_f, a)) and
-                a != a.endswith(".mdf")]
+                a != a.endswith(".csv")]
 
             for a in alist: 
                 if a.startswith(".incomplete_"):
@@ -40,13 +39,11 @@ def archiving_check():
 
             alist_count = len(alist)
 
-            if alist_count > 5: 
-                print(f"LIST LENGTH:  {alist_count} ")
+            if alist_count > 10: 
                 cycle_count += 1
-                print(f"CYCLE COUNT:  {cycle_count} ")
 
                 if cycle_count == 0: 
-                    pause_msg = f"Fold Sets archiving: {alist_count}\n\
+                    pause_msg = f"Folder Sets archiving: {alist_count}\n\
                                   Script will pause while archive queue clears."
                 elif (cycle_count%5 == 0
                     and cycle_count != 30):
@@ -67,10 +64,10 @@ def archiving_check():
                 time.sleep(90)
                 continue
             else: 
-                queue_clear_msg = f"Archiving Queue has cleared. \n\
+                queue_clear_msg = f"Archiving Queue status. \n\
                                     Current active archive count: {alist_count}\n\
                                     Processing of new sets was paused for a total of {cycle_count*300}.seconds.\n\
-                                    The script will now continue."
+                                    Archive submission will proceed."
                 logger.info(queue_clear_msg)
                 queue_status = 0
                 return queue_status
