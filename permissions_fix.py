0b9d27715cea69a8186f0ff5ef2e001cd5fce6a4
Steven Cucolo <8864661+scuc@users.noreply.github.com>
2022-07-14 17:16:40 -0400

    Adds a condition to skip an Isilon2 drop folder locations

--- permissions_fix.py
+++ permissions_fix.py
@@ -1,4 +1,3 @@
-
 import logging
 import os
 import subprocess
@@ -6,7 +5,7 @@ import subprocess
 import config
 
 config = config.get_config()
-script_root = config['paths']['script_root']
+script_root = config["paths"]["script_root"]
 
 logger = logging.getLogger(__name__)
 
@@ -15,20 +14,24 @@ def chmod_chown(drop_f):
     error = False
     start_msg = f"\n\n=========================== START PERMISSIONS FIX ==========================="
     logger.info(start_msg)
-    for folder in drop_f: 
-        try: 
-            os.chdir(script_root)
-            # print(f"======== PATH: {folder}  ========")
-            capturedoutput = subprocess.run(['sudo','./permissions_fix.sh', folder],
-                                            shell=False,
-                                            capture_output=True, 
-                                            universal_newlines=True,
-                                            check=True,
-                                            timeout=120,
-                                            bufsize=100)
-            logger.info(f"\n\\ {capturedoutput} \n\\ ")
+    for folder in drop_f:
+        try:
+            if "Isilon2" in folder.split("\\"):
+                continue
+            else:
+                os.chdir(script_root)
+                capturedoutput = subprocess.run(
+                    ["sudo", "./permissions_fix.sh", folder],
+                    shell=False,
+                    capture_output=True,
+                    universal_newlines=True,
+                    check=True,
+                    timeout=120,
+                    bufsize=100,
+                )
+                logger.info(f"\n\\ {capturedoutput} \n\\ ")
 
-        except Exception as e: 
+        except Exception as e:
             perm_err_msg = f"Error on file permissions subprocess: \n {e}"
             logger.error(perm_err_msg)
             error = True
@@ -38,5 +41,5 @@ def chmod_chown(drop_f):
     return "done"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     chmod_chown()
