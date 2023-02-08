81e3ca35c362ef172801ddd4d708070a590e5407
Steven Cucolo <8864661+scuc@users.noreply.github.com>
2023-02-08 13:21:44 -0500

    Initial commit, standalone script to check objects against the DB, and delete the dups from the Filesystem

---
+++ check_objs_against_db.py
@@ -0,0 +1,128 @@
+import json
+import os
+import logging
+import pprint
+import shutil
+import time
+
+import config as cfg
+import api_DIVA as api
+
+config = cfg.get_config()
+
+root_path = config["paths"]["script_root"]
+mac_root_folders = config["paths"]["mac_root_path"]
+archive_folders = [
+    os.path.join(x, config["paths"]["archiving"]) for x in mac_root_folders
+]
+obj_category = config["DIVA_Obj_Category"]
+source_dest = config["DIVA_Source_Dest"]
+
+logger = logging.getLogger(__name__)
+
+
+def get_archived_objects():
+    """
+    for each watch folder, create a list of directories present, check to see
+    if the same directories are not already present in _archiving and
+    _incomplete and a size larger than 0 KB.
+    """
+    duplicate_dict = {}
+    unique_dict = {}
+    index = 0
+    for archivefolder in archive_folders:
+
+        source_destination = source_dest[index]
+
+        if source_destination == "Isilon2_Archive":
+            volume_name = archivefolder[9:16]
+        else:
+            volume_name = archivefolder[9:17]
+
+        check_df_msg = f"Checking archive folder on: {volume_name}"
+        logger.info(check_df_msg)
+
+        df_delimiter_msg = f"\n\n=============== ARCHIVE FOLDER: {volume_name} ==================\n\n"
+        logger.info(df_delimiter_msg)
+        print(df_delimiter_msg)
+
+        dir_list = [
+            d
+            for d in os.listdir(archivefolder)
+            if os.path.isdir(os.path.join(archivefolder, d))
+        ]
+
+        file_list = [
+            f
+            for f in os.listdir(archivefolder)
+            if os.path.isfile(os.path.join(archivefolder, f))
+            and f.endswith(".mov")
+            or f.endswith(".mxf")
+            or f.endswith(".xml")
+        ]
+
+        archive_list = dir_list + file_list
+
+        if len(archive_list) == 0:
+            empty_msg = f"{volume_name} = No objects in _archiving to check"
+            print(empty_msg)
+            logger.info(empty_msg)
+            duplicate_dict.update({volume_name: []})
+            unique_dict.update({volume_name: []})
+            index += 1
+            continue
+
+        else:
+            duplicate_list = []
+            unique_list = []
+            try: 
+                for objname in archive_list:
+                    status = api.file_check(objname)
+                    if status is True: 
+                        duplicate_list.append(objname)
+                        delete_obj(archivefolder, objname)
+                    else:
+                        unique_list.append(objname)
+                duplicate_dict.update({volume_name: duplicate_list})
+                unique_dict.update({volume_name: unique_list })
+
+            except Exception as e: 
+                logger.error(f"Exception on db check: \n{e}")
+
+    t = time.time()
+    date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
+    combined_dict = {"datestamp": date, "DUPLICATES": duplicate_dict, "UNIQUE": unique_dict }
+    # print(combined_dict)
+
+    os.chdir(root_path)
+    with open("_obj_check_log.json", "r") as f:
+        data = json.load(f)
+        f.close
+
+        data["logs"].append(combined_dict)
+
+    with open("_obj_check_log.json", "w") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+        f.close
+
+
+def delete_obj(archivefolder, objname):
+
+    path = os.path.join(archivefolder, objname)
+    isdir = os.path.isdir(path)
+    isfile = os.path.isfile(path)
+
+    print(objname, isdir, isfile)
+
+    if isdir is True: 
+        shutil.rmtree(path)
+    elif isfile is True: 
+        os.remove(path)
+    else: 
+        print(f"UNABLE TO DETERMINE OBJ TYPE- DIR or FILE")
+
+    return
+
+
+if __name__ == "__main__":
+    get_archived_objects()
