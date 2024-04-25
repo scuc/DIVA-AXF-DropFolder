23a04f1b7d925c1c1491cc4c66e1912f3ecf0ece
Steven C <8864661+scuc@users.noreply.github.com>
2024-04-25 16:58:42 -0400

    Code clean up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#! /usr/bin/env python3
-
-
 import logging
+
+#! /usr/bin/env python3
 import logging.config
 import os
 from datetime import datetime
@@ -15,21 +14,12 @@ import config
 import dropfolder_check_csv as dfc
 import permissions_fix as permissions
 
-config = config.get_config()
-
-script_root = config["paths"]["script_root"]
-mac_root_folders = config["paths"]["mac_root_path"]
-drop_folders = [
-    os.path.join(x, config["paths"]["drop_folder"]) for x in mac_root_folders
-]
-
-logger = logging.getLogger(__name__)
-
 
 def set_logger():
     """
     Setup logging configuration
     """
+    script_root = config.get_config()["paths"]["script_root"]
     path = os.path.join(script_root, "logging.yaml")
 
     with open(path, "rt") as f:
@@ -37,19 +27,13 @@ def set_logger():
 
         # get the file name from the handlers, append the date to the filename.
         for i in config["handlers"].keys():
-            local_datetime = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
-
             if "filename" in config["handlers"][i]:
                 log_filename = config["handlers"][i]["filename"]
                 base, extension = os.path.splitext(log_filename)
                 today = datetime.today()
 
-                log_filename = "{}_{}{}".format(
-                    base, today.strftime("%Y%m%d"), extension
-                )
+                log_filename = f"{base}_{today.strftime('%Y%m%d')}{extension}"
                 config["handlers"][i]["filename"] = log_filename
-            else:
-                continue
 
         logger = logging.config.dictConfig(config)
 
@@ -65,46 +49,36 @@ def main():
         None
     """
 
-    date_start = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
+    logger = set_logger()
 
-    start_msg = f"\n\
-    ================================================================\n\
-                DIVA Archive Script - Start\n\
-                    {date_start}\n\
-    ================================================================\n\
-   "
+    date_start = strftime("%A, %d. %B %Y %I:%M%p", localtime())
+
+    start_msg = f"""
+    ================================================================
+                DIVA Archive Script - Start
+                    {date_start}
+    ================================================================
+    """
 
     logger.info(start_msg)
 
     root_paths = crp.check_root_paths()
 
-    # if root_paths is not False:
-    #     dfc.create_csv()
-    # else:
-    #     pass
-
     if platform == "darwin" and root_paths is not False:
-        p = permissions.chmod_chown(drop_folders)
-    else:
-        p = None
+        permissions.fix_permissions(config.get_config()["paths"]["mac_root_path"])
 
-    if p != "error" or platform != "darwin":
-        dfc.create_csv()
-    else:
-        pass
+    dfc.create_csv()
 
-    date_end = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
+    date_end = strftime("%A, %d. %B %Y %I:%M%p", localtime())
 
-    complete_msg = f"\n\
-    ================================================================\n\
-                DIVA WatchFolder Check - Complete\n\
-                    {date_end}\n\
-    ================================================================\n\
-    "
+    complete_msg = f"""
+    ================================================================
+                DIVA WatchFolder Check - Complete
+                    {date_end}
+    ================================================================
+    """
     logger.info(complete_msg)
-    return
 
 
 if __name__ == "__main__":
-    set_logger()
     main()
--- permissions_fix.py
+++ permissions_fix.py
@@ -10,12 +10,12 @@ script_root = config["paths"]["script_root"]
 logger = logging.getLogger(__name__)
 
 
-def chmod_chown(drop_f):
+def fix_permissions(folders):
     """
     Fix file permissions for the specified folders.
 
     Args:
-        drop_f (list): A list of folders to fix permissions for.
+        folders (list): A list of folders to fix permissions for.
 
     Returns:
         str: A string indicating the completion status of the function.
@@ -25,19 +25,18 @@ def chmod_chown(drop_f):
 
     """
     error = False
-    start_msg = f"\n\n=========================== START PERMISSIONS FIX ==========================="
-    logger.info(start_msg)
-    for folder in drop_f:
+    logger.info(
+        "\n\n=========================== START PERMISSIONS FIX ==========================="
+    )
+    for folder in folders:
         try:
-            if (
-                "Isilon2" in folder.split("/")
-                or "ng-editorial" in folder.split("/")
-                or "fsis3" in folder.split("/")
+            if any(
+                keyword in folder for keyword in ["Isilon2", "ng-editorial", "fsis3"]
             ):
                 continue
             else:
                 os.chdir(script_root)
-                capturedoutput = subprocess.run(
+                captured_output = subprocess.run(
                     ["sudo", "./permissions_fix.sh", folder],
                     shell=False,
                     capture_output=True,
@@ -46,17 +45,18 @@ def chmod_chown(drop_f):
                     timeout=120,
                     bufsize=100,
                 )
-                logger.info(f"\n\\ {capturedoutput} \n\\ ")
+                logger.info(f"\n\\ {captured_output} \n\\ ")
 
         except Exception as e:
-            perm_err_msg = f"Error on file permissions subprocess: \n {e}"
-            logger.error(perm_err_msg)
+            logger.error(f"Error on file permissions subprocess: \n {e}")
             error = True
 
-    end_msg = "\n=========================== END PERMISSIONS FIX ===========================\n"
-    logger.info(end_msg)
+    logger.info(
+        "\n=========================== END PERMISSIONS FIX ===========================\n"
+    )
     return "done"
 
 
 if __name__ == "__main__":
-    chmod_chown()
+    folders_to_fix = []  # Add the folders you want to fix permissions for
+    fix_permissions(folders_to_fix)
